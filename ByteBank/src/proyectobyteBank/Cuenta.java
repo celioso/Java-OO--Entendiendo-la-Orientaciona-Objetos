f79a7c2fc8e9683e9039b3e52ffeface6b76f3ca
Mario Celis <celioso1@hotmail.com>
2023-06-25 14:49:28 -0500

    add new PruebaMetods file

--- ByteBank/src/proyectobyteBank/Cuenta.java
+++ ByteBank/src/proyectobyteBank/Cuenta.java
@@ -7,5 +7,24 @@ class Cuenta{
 	int agencia;
 	int numero;
 	String titular;
+	
+	// Noretorna valor
+	public void depositar(double valor){
+		//Esta cuenta	Esta Cuenta	
+		//this account 	this account
+		//this object
+		this.saldo = this.saldo + valor;
+	}
+	
+	//retorna valor
+	public boolean retirar(double valor){
+		if (this.saldo >= valor) {
+			this.saldo = this.saldo - valor;
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 }
 
